2595254153f2d458ce0be764f6568797a6138c47
phyoeisandar-PC\phyo ei sandar <phyoeisandar40@gmail.com>
2018-06-24 21:14:30 +0630

    FirstTestingResult

--- src/main/java/com/pesandar/model/entities/MCCMain.java
+++ src/main/java/com/pesandar/model/entities/MCCMain.java
@@ -7,15 +7,24 @@ import javax.persistence.Persistence;
 public class MCCMain {
 
 	public static void main(String[] args) {
-		MedicalCheckCenter mcc=new MedicalCheckCenter();
-		mcc.setPart("Head");
-		mcc.setDuration("2minutes");
-		mcc.setSideaffect("99.999 % safe");
-		mcc.setCost("$ 4000");
-		EntityManagerFactory emf=Persistence.createEntityManagerFactory("GithubTesting");
-		EntityManager em=emf.createEntityManager();
+		MedicalCheckCenter mcc1 = new MedicalCheckCenter();
+		mcc1.setPart("Heart");
+		mcc1.setDuration("5minutes");
+		mcc1.setSideaffect("89.999 % safe");
+		mcc1.setCost("$ 900");
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("GithubTesting");
+		EntityManager em = emf.createEntityManager();
+		MedicalCheckCenter mcc=em.find(MedicalCheckCenter.class,1);
 		em.getTransaction().begin();
-		em.persist(mcc);
+	
+		mcc.setPart("Excretory System");
+		em.persist(mcc1);
+		em.remove(mcc);
+		/*System.out.println(mcc.getPart());
+		System.out.println(mcc.getDuration());
+		System.out.println(mcc.getSideaffect());
+		System.out.println(mcc.getCost());*/
 		em.getTransaction().commit();
 		em.close();
 	}
